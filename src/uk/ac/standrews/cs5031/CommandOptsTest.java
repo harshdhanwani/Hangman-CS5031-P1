d8366795bd9269aa33eb360813c0af2f7401b4fa
harshdhanwani <harsh.dhanwani112yahoo.in>
2018-02-20 22:38:55 +0000

    Added two more tests for different usage scenarios through command line in CommandOptsTest.java class.

--- src/uk/ac/standrews/cs5031/CommandOptsTest.java
+++ src/uk/ac/standrews/cs5031/CommandOptsTest.java
@@ -15,6 +15,28 @@ public class CommandOptsTest {
 		assertEquals(opts.wordsource, "words.txt");
 	}
 
+	// if some arguments are missing
+	@Test
+	public void missingOptionsTest(){
+		String[] args = { "--guesses", "--hints", "5", "words.txt" };
+		CommandOpts opts = new CommandOpts(args);
+		// default number of guesses set to set in the program.
+		assertEquals(opts.maxGuesses, 10);
+		assertEquals(opts.maxHints, 5);
+		assertEquals(opts.wordsource, "words.txt");
+	}
+
+	// if all arguments are missing
+    @Test
+    public void noOptionsTest(){
+        String[] args = {};
+        CommandOpts opts = new CommandOpts(args);
+        // default number of guesses set to set in the program.
+        assertEquals(opts.maxGuesses, 10);
+        // default number of hints set to set in the program.
+        assertEquals(opts.maxHints, 5);
+        assertEquals(opts.wordsource, "");
+    }
 
 
 }
