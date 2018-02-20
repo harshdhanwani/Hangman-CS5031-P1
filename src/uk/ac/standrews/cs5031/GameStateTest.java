a1e814871ab301d5cdc7a8f56751acbd11046a93
harshdhanwani <harsh.dhanwani112yahoo.in>
2018-02-20 17:14:08 +0000

    Added tests T4 & T5 to validate remaining number of guesses either by letters or word.

--- src/uk/ac/standrews/cs5031/GameStateTest.java
+++ src/uk/ac/standrews/cs5031/GameStateTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 public class GameStateTest {
 
+    // T1
 	@Test
 	public void gameStateTest() {
 			GameState gameState = new GameState("apple", 10, 5);
@@ -17,7 +18,7 @@ public class GameStateTest {
 			assertEquals(gameState.numberOfHints, 5);
 	}
 
-	// test to validate letter guessed.
+	// T2 - test to validate letter guessed.
 	@Test
 	public void validateGuessLetterTest(){
 		GameState gameState = new GameState("apple", 10, 5);
@@ -25,13 +26,36 @@ public class GameStateTest {
 		assertTrue(gameState.validateGuessedLetter('p'));
 	}
 
-	// test to validate word guessed.
+	// T3 - test to validate word guessed.
 	@Test
 	public void validateGuessWordTest(){
 		GameState gameState = new GameState("apple", 10, 5);
 		assertFalse(gameState.validateGuessedWord("addle"));
 		assertTrue(gameState.validateGuessedWord("apple"));
 	}
+
+	// T4 - validate remaining guesses after letter is entered ( one correct guess)
+	@Test
+	public void validateRemainingGuesses(){
+		GameState gameState = new GameState("apple", 10, 5);
+		gameState.validateGuessedLetter('w');
+		gameState.validateGuessedLetter('g');
+		gameState.validateGuessedLetter('p');
+		assertEquals(gameState.chancesLeft, 8);
+	}
+
+	// T5 - validate remaining guesses after a word is entered (one correct word guess)
+	@Test
+	public void validateRemainingGuessesWord(){
+		GameState gameState = new GameState("apple", 10, 5);
+		gameState.validateGuessedWord("hello");
+		gameState.validateGuessedWord("america");
+		gameState.validateGuessedWord("ball");
+		gameState.validateGuessedWord("apple");
+		assertEquals(gameState.chancesLeft, 7);
+	}
+
+
 
 
 
