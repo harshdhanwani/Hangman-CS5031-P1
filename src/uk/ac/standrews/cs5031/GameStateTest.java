7f26fac960063f52fba1ac6950a4f3aabb1dea76
harshdhanwani <harsh.dhanwani112yahoo.in>
2018-02-20 17:02:08 +0000

    Added Tests (T2) to validate letter guesses and (T3) to validate word guesses.

--- src/uk/ac/standrews/cs5031/GameStateTest.java
+++ src/uk/ac/standrews/cs5031/GameStateTest.java
@@ -17,5 +17,22 @@ public class GameStateTest {
 			assertEquals(gameState.numberOfHints, 5);
 	}
 
+	// test to validate letter guessed.
+	@Test
+	public void validateGuessLetterTest(){
+		GameState gameState = new GameState("apple", 10, 5);
+		assertFalse(gameState.validateGuessedLetter('d'));
+		assertTrue(gameState.validateGuessedLetter('p'));
+	}
+
+	// test to validate word guessed.
+	@Test
+	public void validateGuessWordTest(){
+		GameState gameState = new GameState("apple", 10, 5);
+		assertFalse(gameState.validateGuessedWord("addle"));
+		assertTrue(gameState.validateGuessedWord("apple"));
+	}
+
+
 
 }
